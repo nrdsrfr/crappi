e066c19d826d490c957332abb774a25141a6be62
Jesse Cary <jesse.cary@bedegaming.com>
2014-05-22 16:13:15 +0100

    added config option for first function and test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-crappi
+crappi 0.2.0
 ======
 
 A failure testing utility to make your Hapi API more Crappi
@@ -6,3 +6,9 @@ A failure testing utility to make your Hapi API more Crappi
 The impetus for this plugin was an internal request to help my ops team debug an issue by randomly increasing API response times by 10 seconds to force a client to timeout.
 
 I hope to add more functionality to make this more useful, such as adding new ways to make your API crappi, using tags to specify which routes to apply crappiness to and parameters to adjust the crapiness
+
+### Plugin Options
+
+- `slomo` - options for slowing down an API's response
+    - `rate` - percent of calls affected
+    - `time` - number of seconds to delay the response
---
+++ test/index.js
@@ -0,0 +1,60 @@
+'use strict';
+var Hapi = require('hapi');
+var Lab = require('lab');
+
+var expect = Lab.expect;
+var before = Lab.before;
+var beforeEach = Lab.beforeEach;
+var after = Lab.after;
+var afterEach = Lab.afterEach;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+
+
+Lab.test('returns true when 1 + 1 equals 2', function (done) {
+
+  Lab.expect(1 + 1).to.equal(2);
+  done();
+});
+
+
+describe('Crappi', function () {
+
+  it('delays API calls by a specified time at a specified rate', function (done) {
+
+    var server = Hapi.createServer(0);
+
+    server.route(
+      [{
+        method: 'GET',
+        path: '/test',
+        handler: function (request, reply) {
+          reply('success');
+        }
+      }]
+    );
+
+    server.pack.require({ '../': {
+        slomo: {
+        rate: 100, // percent of calls affected
+        time: 1  // number of seconds to delay the response
+      }
+    }}, function (err) {
+      if (err) { done(err); }
+      else {
+        console.log('started server ', server.info);
+        server.start(function () {
+          var start = new Date();
+          server.inject('/test', function (res) {
+            var millis = Math.abs(new Date() - start);
+            expect(millis).to.be.greaterThan(1000);
+            expect(res.payload).to.equal('success');
+            done();
+          });
+        });
+      }
+    });
+
+  });
+});
